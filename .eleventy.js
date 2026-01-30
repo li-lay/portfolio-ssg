5749eaeb14046895dc8c88da06f13c72f0dfff4f
LILAY <contact@lilay.dev>
2026-01-30 11:43:39 +0700

    feat: improve SEO

--- .eleventy.js
+++ .eleventy.js
@@ -4,8 +4,10 @@ module.exports = function (eleventyConfig) {
   // Copy CSS files to the output directory
   eleventyConfig.addPassthroughCopy({ "src/css": "css" });
 
-  // Copy favicon
+  // Copy favicon and SEO files
   eleventyConfig.addPassthroughCopy("src/favicon.ico");
+  eleventyConfig.addPassthroughCopy("src/robots.txt");
+  eleventyConfig.addPassthroughCopy("src/sitemap.xml");
 
   // Set HTML as the default template engine
   eleventyConfig.setTemplateFormats([
